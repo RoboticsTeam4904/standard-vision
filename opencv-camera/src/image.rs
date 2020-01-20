a0f8ebd6086a9ad3f80bbf8754cc3d4cac2cb6ed
aemino <aeminodev@gmail.com>
2020-01-20 13:56:15 -0800

    Fix AsMat implementation

--- opencv-camera/src/image.rs
+++ opencv-camera/src/image.rs
@@ -1,3 +1,4 @@
+use ndarray::prelude::*;
 use opencv::prelude::*;
 
 use standard_vision::{traits::ImageData, types::Image};
@@ -9,11 +10,27 @@ pub trait AsMat {
 
 impl<'a, I: ImageData> AsMat for Image<'a, I> {
     fn as_mat(&self) -> Mat {
-        unsafe { Mat::from_raw_ptr(self.as_pixels().as_ptr() as *mut std::ffi::c_void) }
+        let pixels = self.as_pixels();
+
+        Mat::new_rows_cols_with_data(
+            pixels.len_of(Axis(0)) as i32,
+            pixels.len_of(Axis(1)) as i32,
+            opencv::core::CV_8UC3,
+            unsafe { &mut *(pixels.first().unwrap() as *const u8 as *mut std::ffi::c_void) },
+            3,
+        ).unwrap()
     }
 
     fn as_mat_mut(&mut self) -> Mat {
-        unsafe { Mat::from_raw_ptr(self.as_pixels().as_ptr() as *mut std::ffi::c_void) }
+        let mut pixels = self.as_pixels_mut();
+
+        Mat::new_rows_cols_with_data(
+            pixels.len_of(Axis(0)) as i32,
+            pixels.len_of(Axis(1)) as i32,
+            opencv::core::CV_8UC3,
+            unsafe { &mut *(pixels.first_mut().unwrap() as *mut u8 as *mut std::ffi::c_void) },
+            3,
+        ).unwrap()
     }
 }
 
